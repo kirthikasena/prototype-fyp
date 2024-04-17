c084ef8d74bd3c74b70381b66c777ba76115193e
kirthisena <kirthisenathiraja@gmail.com>
2024-04-17 15:53:56 +0530

    Update for deployment

--- my-backend/index.js
+++ my-backend/index.js
@@ -5,7 +5,7 @@ const { exec } = require('child_process');
 const fs = require('fs');  // Required for file operations
 const app = express();
 // const port = 5000;
-
+const path = require('path');
 
 const PORT = process.env.PORT || 5000; 
 // Setup CORS
